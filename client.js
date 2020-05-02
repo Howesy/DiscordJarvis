1a49ec107de81cdc54151d3809c41ee308659e07
Howesy <howesybusiness@gmail.com>
2020-05-02 01:59:14 +0100

    Initial Commit

---
+++ client.js
@@ -0,0 +1,61 @@
+//Declare our required libs.
+const fs = require("fs");
+const discord = require("discord.js");
+
+//Declare our directories and deconstruct them into various variables for easy access.
+const botDirectories = ["./events/", "./settings/", "./commands/user/", "./commands/developer/"];
+const [eventsDir, settingsDir, userCommandsDir, developerCommandsDir] = botDirectories;
+
+//Declare our "bot" (Client) variable and assign it prototype properties so we can easily store and access various imporant collections.
+const bot = new discord.Client();
+//Assign Collections to our commands and alias property prototypes on the "bot" variable.
+bot.commands = new discord.Collection();
+bot.aliases = new discord.Collection();
+//Assign our JSON settings file to a property prototype on the "bot" variable, so we can easily access our settings on the go.
+bot.settings = require(`${settingsDir}settings.js`);
+
+//Read the events directory, require the event file and initialize it with our bot variable to start listening to that event.
+fs.readdir(eventsDir, function(error, events) {
+    events.forEach(function(event) {
+        if (!event.endsWith(".js"))
+            return;
+        const eventModule = require(`${eventsDirectory}${event}`);
+        const [eventName] = event.split(".")[0];
+        console.log(`DiscordJarvis | Event Allocated => ${eventName}`);
+        bot.on(eventName, (...constructs) => eventModule.run(bot, ...constructs));
+    });
+});
+
+//Call the "ReadCommands" function to read the various command directories that we have.
+//We segregate commands dependant on category so we can view them easier later in our help command.
+ReadCommands(userCommandsDir);
+ReadCommands(developerCommandsDir);
+
+/*
+Decleration of our "ReadCommands" function, like when we read our events, this function stores the command names and required files
+into a collection for easy access so we can check for and run them at the users request later on.
+We also assign aliases to command names so people can use aliases for easier access to their designated commands.
+*/
+
+/**
+ * Read command files within a specified directory and assign them to the bot.commands prototype property.
+ * @param {string} commandDirectory
+ * @returns {void} void
+ */
+
+function ReadCommands(commandDirectory) {
+    fs.readdir(commandDirectory, function(error, commands) {
+        if (error) throw new Error(error);
+        commands.forEach(function(command) {
+            if (!command.endsWith(".js")) 
+                return;
+            const commandFile = require(`${commandDirectory}${command}`);
+            const commandName = commandFile.info.name;
+            bot.commands.set(commandName, commandFile);
+            console.log(`DiscordJarvis | Command Allocated => ${commandName}`);
+            commandFile.configuration.aliases.forEach(function(alias) {
+                bot.aliases.set(alias, commandName);
+            });
+        });
+    });
+}
---
+++ events/ready.js
@@ -0,0 +1,20 @@
+/*
+When initializing an event, we call a method "run" with the parameters (bot, ...constructs).
+Essentially, if we simplify this down. All we're doing is reading a function from a file and using it as our function for event.
+Through each event file or "module", as I'll be calling them we pass bot and any other parameters we'll be using for the event.
+Since this is the "ready" event and as specified by the discord.js documentation there are no other parameters, we can just pass bot
+in this instance.
+*/
+
+/**
+ * Method for running exported module.
+ * @param {object} bot This is our bot (Client) variable.
+ */
+
+//Declaring a function "run", and exporting it.
+exports.run = function(bot) {
+    //We're deconstructing the bot object and reading the guilds property only as we don't need the entire object.
+    const {guilds} = bot;
+    //Console logging how many guilds the bot is located in, and that the bot is ready to be commanded.
+    console.log(`Hello sir, I have ${guilds.size} at my disposal and I'm ready at your command.`);
+}
